1904b4f0294b136cfd1b82d81321d1285c50cdf2
Paul <aguilarpgc@gmail.com>
2015-02-27 00:01:14 -0500

    updated bash file

--- list_files.sh
+++ list_files.sh
@@ -1,8 +1,8 @@
 echo "# SPOJ
 SPOJ problems.
-"
+" > "README.md"
 
 for f in *.cpp
 do
-	echo "* [${f%.*}](http://www.spoj.com/problems/${f%.*})"
+	echo "* [${f%.*}](http://www.spoj.com/problems/${f%.*})" >> "README.md"
 done
